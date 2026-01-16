733b7e1dfdd19d600fb8661613b9dee4bc73cb6c
pawtut <49070810+pawtut@users.noreply.github.com>
2026-01-16 18:21:17 +0000

    Add task fetching and a new npm script

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,6 +6,9 @@
                 <li>
                     <NuxtLink to="/personalizer">Project 1: Personalizer (TS + API)</NuxtLink>
                 </li>
+                <li>
+                    <NuxtLink to="/tasks">Project 2: Tasks (TS + API)</NuxtLink>
+                </li>
             </ul>
         </nav>
     </div>
--- app/pages/tasks.vue
+++ app/pages/tasks.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
-/**
- * TODO: To implement response for tasks and put them in a list
- */
+interface TaskResponse {
+    id: string;
+    title: string;
+    completed: boolean;
+}
+
+const completed = ref(false);
+
+const { data: tasks, pending, refresh } = await useFetch<TaskResponse[]>('/api/tasks');
 </script>
 
 <template>
@@ -9,5 +15,12 @@
         <NuxtLink to="/">Back Home</NuxtLink>
         <h1>Tasks list</h1>
         <!-- TODO: Tasks to list here -->
+        <div v-if="pending">Loading tasks...</div>
+        <ul v-else>
+            <li v-for="task in tasks" :key="task.id">
+                <h2>{{ task.title }}</h2>
+                <p>Status: {{ task.completed ? '✅' : '⭕' }}</p>
+            </li>
+        </ul>
     </div>
 </template>
