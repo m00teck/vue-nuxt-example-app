d0f82625ec80b91f7d0b953c13b0a75245e06290
pawtut <49070810+pawtut@users.noreply.github.com>
2026-01-20 19:27:07 +0000

    Format tasks types

--- shared/tasks.ts
+++ shared/tasks.ts
@@ -6,17 +6,13 @@ export interface Task {
   completed: boolean;
 }
 
-// We can also define what's needed to create or update a task
-export type CreateTaskInput = Pick<Task, 'title'>;
-export type UpdateTaskInput = Partial<Pick<Task, 'title' | 'completed'>>;
+export type CreateTaskInput = Pick<Task, "title">;
+export type UpdateTaskInput = Partial<Pick<Task, "title" | "completed">>;
 
-
-// Define what the frontend MUST send us
 export interface CreateTaskBody {
   title: string;
 }
 
-// Define what the server WILL return
 export interface TaskResponse {
   id: string;
   title: string;
