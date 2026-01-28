3f7849d08b60e6dcc9ecb3df932aa79c5d133113
pawtut <49070810+pawtut@users.noreply.github.com>
2026-01-28 19:27:16 +0000

    More endpoints for tasks

--- server/api/tasks/[id].delete.ts
+++ server/api/tasks/[id].delete.ts
@@ -1,17 +1,34 @@
+import { eq } from "drizzle-orm";
+import { TaskErrorRequest } from "~~/shared/types/tasks";
+
 export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, "id");
 
-  const taskExists = true;
-
-  if (!taskExists) {
-    throw createError({
-      statusCode: 404,
-      statusMessage: `Task with ID ${id} not found`,
+  if (!id) {
+    throw createError<TaskErrorRequest>({
+      statusCode: 400,
+      statusMessage: "Task ID is required",
     });
   }
 
-  return {
-    success: true,
-    message: `Task ${id} has been permanently deleted`,
-  };
+  try {
+    const [deletedTask] = await db
+      .delete(tasks)
+      .where(eq(tasks.id, id))
+      .returning();
+
+    if (!deletedTask) {
+      throw createError<TaskErrorRequest>({
+        statusCode: 404,
+        statusMessage: "Task not found",
+      });
+    }
+
+    return deletedTask;
+  } catch (e) {
+    throw createError<TaskErrorRequest>({
+      statusCode: 500,
+      statusMessage: "An error occurred while updating the task",
+    });
+  }
 });
--- server/api/tasks/[id].patch.ts
+++ server/api/tasks/[id].patch.ts
@@ -1,12 +1,36 @@
-import { Task, UpdateTaskInput } from "~~/shared/tasks";
+import { Task, UpdateTaskInput, TaskErrorRequest } from "~~/shared/types/tasks";
+import { eq } from "drizzle-orm";
 
 export default defineEventHandler(async (event): Promise<Task> => {
   const id = getRouterParam(event, "id");
   const body = await readBody<UpdateTaskInput>(event);
 
-  return {
-    id: id || "unknown",
-    title: "Existing Task Title",
-    completed: body.completed ?? false,
-  };
+  if (!id) {
+    throw createError<TaskErrorRequest>({
+      statusCode: 400,
+      statusMessage: "Task ID is required",
+    });
+  }
+
+  try {
+    const [updatedTask] = await db
+      .update(tasks)
+      .set(body)
+      .where(eq(tasks.id, id))
+      .returning();
+
+    if (!updatedTask) {
+      throw createError<TaskErrorRequest>({
+        statusCode: 404,
+        statusMessage: "Task not found",
+      });
+    }
+
+    return updatedTask;
+  } catch (e) {
+    throw createError<TaskErrorRequest>({
+      statusCode: 500,
+      statusMessage: "An error occurred while updating the task",
+    });
+  }
 });
